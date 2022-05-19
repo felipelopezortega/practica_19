a8b2dc9517c5b49965403cb6121332209e39b7b7
visita <visita@TELEMATICA.UCOL.MX>
2022-05-19 08:32:13 -0500

    The code is now documented

--- routes/persons.js
+++ routes/persons.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const mongoose = require("../node_modules/mongoose");
-let Person = require("../models/person");
+const express = require("express"); //se inyecta la dependencia de express
+const router = express.Router(); // se crea una constante de una propiedad de express, router
+const mongoose = require("../node_modules/mongoose"); // se inyecta la dependencia de mongoose, la base de datos
+let Person = require("../models/person"); //se define una variable "Person"
 
+//primer ruta, para mostrarnos el contenido de nuestra base de datos
 router.get("/persons", function(req,res, next){
     Person.find(function(err,persons){
         if(err) return next(err);
@@ -10,11 +11,13 @@ router.get("/persons", function(req,res, next){
     });
 })
 
+//segunda ruta, se renderiza index, el cual contiene el formulario
 router.get("/index", function(req,res){
 
     res.render("index");
 });
 
+//De manera preestablecida, se establecen valores a los atributos que recibe el formulario
 router.post("/personssend", async (req, res) => {
 
     let felipe = new Person ({
